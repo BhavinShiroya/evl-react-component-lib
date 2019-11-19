b6ef018f04a6fd26c5ea1ac69e30d55c4b896be2
BhavinShiroya <bhavin@everledger.io>
2019-11-19 17:23:11 +0530

    Updates

---
+++ precache-manifest.d200c07d6806e47355514413a1f4c374.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91090fd7622ceeb6d8b0896ff4a20955",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e0a4c92709f35a0b01d",
+    "url": "/static/css/main.462b0bd9.chunk.css"
+  },
+  {
+    "revision": "ef7f1de123df56ba39c4",
+    "url": "/static/js/2.2439447e.chunk.js"
+  },
+  {
+    "revision": "4e0a4c92709f35a0b01d",
+    "url": "/static/js/main.37c5c78f.chunk.js"
+  },
+  {
+    "revision": "84d6721e64a18fe343e6",
+    "url": "/static/js/runtime-main.f47b6839.js"
+  }
+]);
